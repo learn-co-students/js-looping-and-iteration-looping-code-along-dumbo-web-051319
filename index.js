c851ae28bf14da2eaac10251daf7b33f2c2d520a
Stefanos <stefanosu422@gmail.com>
2019-06-23 11:33:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+// const cards = ['Lisa', 'Kaitlin', 'Jan' ]; 
+
+function writeCards (namesArr, num) {
+    let cards = []; 
+    for (let i = 0; i < namesArr.length; i++) {
+    cards.push(`Thank you, ${namesArr[i]}, for the wonderful ${num} gift!`)
+    }
+    return cards; 
+}  
+
+
+
+function countdown(countdown) {
+    // let donuts = 10;     
+    while ( countdown >= 0 ) {
+      console.log( countdown--);
+    }
+  }
+  countdown(10);
+
+
+  
