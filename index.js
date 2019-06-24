59834518c3553d740eee9229f5602cc3f15d08b2
Yonas Fesehatsion <yone920@gmail.com>
2019-06-24 13:35:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+const given = ["Ada", "Brendan", "Ali"];
+const string = "birthday";
+
+function writeCards(names, string) {
+    arr = [];
+    for (let i = 0; i < names.length; i++) {
+       arr.push(`Thank you, ${names[i]}, for the wonderful ${string} gift!`);
+    }
+    return arr;
+}
+
+writeCards(given, string);
+
+
+function countdown(number) {
+    while (number >= 0) {
+        console.log(number)
+        number--;
+    }
+}
+
+countdown(10);
