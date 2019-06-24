dc91b09c7adc309154cc799d524ae9c0d38d633e
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-24 01:38:57 -0400

    created functions using for and while loops

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+  let greeting = [];
+  for (let i = 0; i < array.length; i++) {
+    greeting.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return greeting;
+}
+
+function countdown(integer) {
+  while (integer > 0) {
+    console.log(integer);
+    integer -= 1;
+  }
+  console.log(integer);
+}
