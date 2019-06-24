a7d08074899aaefe719e45cf3d7efabd5040ceec
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-24 18:30:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, events) {
+  let array = [];
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+
+  }
+  return array;
+}
+
+function countdown(number) {
+  while (number > -1) {
+    console.log(number);
+    number= number - 1;
+  }
+}
