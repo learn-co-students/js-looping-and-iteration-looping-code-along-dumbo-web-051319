e30cba26b7e93b5cbedebf20eb6ae731b16aa33e
Toni Lamont <tlam212@gmail.com>
2019-06-24 19:30:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, day) {
+  let newArray = [];
+  for(let i = 0; i < names.length; i++){
+    newArray.push( `Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+  }
+  return newArray;
+}
+
+function countdown(num){
+let counter = num;
+while (counter >= 0){
+  console.log(counter--);
+}
+}
