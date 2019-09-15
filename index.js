8e537c1bda8a18b62bf191682955cbb53908912d
othaderek <otha.hernandez@gmail.com>
2019-09-15 00:00:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+// "Thank you, Ali, for the wonderful birthday gift."
+
+const writeCards = (array, eventName) => {
+  let result = [];
+  for (let i = 0; i < array.length; i++){
+    let message = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`
+    result.push(message);
+  }
+  return result
+}
+
+const countdown = (num) => {
+  while (num >= 0){
+    console.log(num);
+    num--
+  }
+}
+countdown(5);
